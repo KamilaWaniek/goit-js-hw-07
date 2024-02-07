47044965f392935da0fa10fb811c488b51ee05c9
KamilaWaniek <k.scheffera@wp.pl>
2024-02-07 14:46:37 +0100

    Update 01-gallery.js

--- 01-gallery.js
+++ 01-gallery.js
@@ -1,63 +1,23 @@
 import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
 const gallery = document.querySelector(".gallery");
 
-const galleryElements = galleryItems.map((item) => {
-  const galleryItem = document.createElement("div"); //tworzę nowy element
-  galleryItem.classList.add("gallery__item"); //dodaję nową klasę elementu
+const fixedGalleryItems = galleryItems.map(
+  (galleryItem) =>
+    `<li class="gallery__item">
+      <a class="gallery__link" href="${galleryItem.original}">
+      <img
+        class="gallery__image"
+        src="${galleryItem.preview}" 
+        alt="${galleryItem.description}"
+        data-source="${galleryItem.original}"
+        >
+        </img>
+        </a>
+     </li>`
+);
+.join("");
 
-  const galleryLink = document.createElement("a");
-  galleryLink.classList.add("gallery__link");
-  galleryLink.href = item.original; //wersja oryginala obrazka
-
-  const galleryImage = document.createElement("img");
-  galleryImage.classList.add("gallery__image");
-  galleryImage.src = item.preview; //wersja min. obrazka
-  galleryImage.dataset.source = item.original;
-  galleryImage.alt = item.description;
-
-  galleryLink.appendChild(galleryImage); //dodaję element galleryLink do node'a galleryImage
-  galleryItem.appendChild(galleryLink);
-
-  return galleryItem;
-});
-
-gallery.append(...galleryElements);
-
-gallery.addEventListener("click", (event) => {
-  event.preventDefault();
-
-  // What if clicked outside the image
-  const isImageClicked = event.target.classList.contains("gallery__image");
-
-  if (isImageClicked) {
-    const lightbox = basicLightbox.create(
-      `
-        <img src="${event.target.dataset.source}" width="800" height="600">
-      `,
-      {
-        onShow: (instance) => {
-          console.log("Lightbox is shown");
-          // Dodaj kod do wykonania po pokazaniu lightboxa
-        },
-        onClose: (instance) => {
-          console.log("Lightbox is closed");
-          // Dodaj kod do wykonania po zamknięciu lightboxa
-          window.removeEventListener("keydown", closeLightboxOnEsc);
-        },
-      }
-    );
-
-    lightbox.show();
-
-    // "ESC" Close)
-    const closeLightboxOnEsc = (e) => {
-      if (e.key === "Escape") {
-        lightbox.close();
-        window.removeEventListener("keydown", closeLightboxOnEsc);
-      }
-    };
-
-    // "ESC" listener close
-    window.addEventListener("keydown", closeLightboxOnEsc);
-  }
-});
+gallery.insertAdjacentHTML("afterbegin", fixedGalleryItems);
+console.log(galleryItems);
