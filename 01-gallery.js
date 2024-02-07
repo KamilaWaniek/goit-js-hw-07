5bbd60af7eca97f8e7c8f4ece1f43f9c780dab46
KamilaWaniek <k.scheffera@wp.pl>
2024-02-07 15:08:51 +0100

    Update 01-gallery.js

--- 01-gallery.js
+++ 01-gallery.js
@@ -1,23 +1,21 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
 const gallery = document.querySelector(".gallery");
 
-const fixedGalleryItems = galleryItems.map(
-  (galleryItem) =>
-    `<li class="gallery__item">
-      <a class="gallery__link" href="${galleryItem.original}">
-      <img
-        class="gallery__image"
-        src="${galleryItem.preview}" 
-        alt="${galleryItem.description}"
-        data-source="${galleryItem.original}"
-        >
-        </img>
-        </a>
-     </li>`
-);
-.join("");
+const listItems = galleryItems
+  .map(
+    (item) =>
+      `<li class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+                <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+            alt="${item.description}"
+            >
+            </a>
+    </li>`
+  )
+  .join("");
 
-gallery.insertAdjacentHTML("afterbegin", fixedGalleryItems);
-console.log(galleryItems);
+gallery.insertAdjacentHTML("afterbegin", listItems);
