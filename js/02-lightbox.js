e7c9470af387b5a29f9b31b6005f445a1637e373
KamilaWaniek <k.scheffera@wp.pl>
2024-02-08 11:17:28 +0100

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
+
+const gallery = document.querySelector(".gallery");
+
+// Dodanie zdjęć z klasami do DOM-u
+
+const galleryList = galleryItems
+  .map(
+    (item) =>
+      `<li class="gallery__item">
+        <a class="gallery__item" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a>
+    </li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", galleryList);
+
+// SimpleLightBox + podpisy pod zdjęciami
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionType: "attr",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
